9d67e116c8844ee2957104899d421ed9f51e0b93
Steve Ryan <steve.ryan@asu.edu>
2020-05-01 12:14:21 -0700

    Add ASU global header. Remove other WP features not needed.

--- inc/Custom_Header/Component.php
+++ inc/Custom_Header/Component.php
@@ -42,38 +42,5 @@ class Component implements Component_Interface {
 	/**
 	 * Adds support for the Custom Logo feature.
 	 */
-	public function action_add_custom_header_support() {
-		add_theme_support(
-			'custom-header',
-			apply_filters(
-				'wp_rig_custom_header_args',
-				[
-					'default-image'      => '',
-					'default-text-color' => '000000',
-					'width'              => 1600,
-					'height'             => 250,
-					'flex-height'        => true,
-					'wp-head-callback'   => [ $this, 'wp_head_callback' ],
-				]
-			)
-		);
-	}
-
-	/**
-	 * Outputs extra styles for the custom header, if necessary.
-	 */
-	public function wp_head_callback() {
-		$header_text_color = get_header_textcolor();
-
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
-
-		if ( ! display_header_text() ) {
-			echo '<style type="text/css">.site-title, .site-description { position: absolute; clip: rect(1px, 1px, 1px, 1px); }</style>';
-			return;
-		}
-
-		echo '<style type="text/css">.site-title a, .site-description { color: #' . esc_attr( $header_text_color ) . '; }</style>';
-	}
+	public function action_add_custom_header_support() {}
 }
--- inc/Customizer/Component.php
+++ inc/Customizer/Component.php
@@ -10,6 +10,7 @@ namespace WP_Rig\WP_Rig\Customizer;
 use WP_Rig\WP_Rig\Component_Interface;
 use function WP_Rig\WP_Rig\wp_rig;
 use WP_Customize_Manager;
+use WP_Customize_Control;
 use function add_action;
 use function bloginfo;
 use function wp_enqueue_script;
@@ -69,14 +70,89 @@ class Component implements Component_Interface {
 		}
 
 		/**
-		 * Theme options.
+		 * ASU global header options panel.
+		 * Allows an end user to specifiy if the site has a parent site and provide a name and an URL if so.
 		 */
+
+		$wp_customize->add_panel(
+			'wordpress_asu_theme_panel',
+			array(
+				'title'      => __( 'ASU Branding Elements', 'wp-rig' ),
+				'priority'   => 1,
+			)
+		);
+
+		$wp_customize->add_setting(
+			'wordpress_asu_theme_options[parent]',
+			array(
+				'default'           => '',
+				'capability'        => 'edit_theme_options',
+				'type'              => 'option',
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Control(
+				$wp_customize,
+				'subsite',
+				array(
+					'label'      => __( 'Does this site have a parent?', 'wp-rig' ),
+					'section'    => 'wordpress_asu_theme_section_subsite_settings',
+					'settings'   => 'wordpress_asu_theme_options[parent]',
+					'type'       => 'checkbox',
+				)
+			)
+		);
+
 		$wp_customize->add_section(
-			'theme_options',
-			[
-				'title'    => __( 'Theme Options', 'wp-rig' ),
-				'priority' => 130, // Before Additional CSS.
-			]
+			'wordpress_asu_theme_section_subsite_settings',
+			array(
+				'title'      => __( 'Global Header', 'wp-rig' ),
+				'priority'   => 10,
+				'panel'      => 'wordpress_asu_theme_panel',
+			)
+		);
+
+		$wp_customize->add_setting(
+			'wordpress_asu_theme_options[parent_site_name]',
+			array(
+				'default'           => '',
+				'capability'        => 'edit_theme_options',
+				'type'              => 'option',
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Control(
+				$wp_customize,
+				'parent_blog_name',
+				array(
+					'label'      => __( 'Parent Site Name', 'wp-rig' ),
+					'section'    => 'wordpress_asu_theme_section_subsite_settings',
+					'settings'   => 'wordpress_asu_theme_options[parent_site_name]',
+				)
+			)
+		);
+
+		$wp_customize->add_setting(
+			'wordpress_asu_theme_options[parent_url]',
+			array(
+				'default'           => '',
+				'capability'        => 'edit_theme_options',
+				'type'              => 'option',
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Control(
+				$wp_customize,
+				'parent_blog_id',
+				array(
+					'label'      => __( 'Parent Site URL', 'wp-rig' ),
+					'section'    => 'wordpress_asu_theme_section_subsite_settings',
+					'settings'   => 'wordpress_asu_theme_options[parent_url]',
+				)
+			)
 		);
 	}
 
